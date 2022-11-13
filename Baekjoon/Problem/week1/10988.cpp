bca2531569c1e2f92e1ef5076519042db145f3a6
yeye921 <yewon921@ajou.ac.kr>
2022-11-13 23:54:15 +0900

    day 23

--- Baekjoon/Problem/week1/10988.cpp
+++ Baekjoon/Problem/week1/10988.cpp
@@ -1,16 +1,15 @@
 // 문자열 거꾸로 읽기 = 문자열 뒤집기 => reverse(first, last)
 #include <bits/stdc++.h>
 using namespace std;
-string s, s2;
+string s, temp;
 int main(){
     cin >> s;
-    s2 = s;
+    temp = s;
 
     reverse(s.begin(), s.end());
 
-    if(s == s2) cout << 1;
-    else cout << 0;
+    if(temp == s) cout << 1 << "\n";
+    else cout << 0 << "\n";
 
     return 0;
-
 }
--- Baekjoon/Problem/week1/1159.cpp
+++ Baekjoon/Problem/week1/1159.cpp
@@ -1,27 +1,27 @@
+// 카운팅은 맵 또는 배열 !
 #include <bits/stdc++.h>
 using namespace std;
-int N, cnt[26];
-string s;
-vector<char> v;
+int N, cnt[26]; // cnt 배열
+string s, ret;
 int main(){
     cin >> N;
 
     for(int i = 0; i < N; i++){
         cin >> s;
-        cnt[s[0] - 97]++; // 아스키코드 활용
+        cnt[s[0] - 97]++; // 아스키코드 활용 (선수의 성이 해당되는 알파벳에 +1) (97대신 'a'도 괜찮음)
     }
 
     // cnt배열 돌면서 같은 성을 가진 선수가 5명 이상인 것 찾기
     for(int i = 0; i < 26; i++){
         if(cnt[i] >= 5){ 
-            v.push_back((char)(i + 97)); // !! 인덱스(i)에다 97을 더해야 원래 아스키코드 문자가 나옴
+            ret += (char)(i + 97); // !! 인덱스(i)에다 97을 더해야 원래 아스키코드 문자가 나옴
         }
     }
 
     if(v.empty()){ // !! 벡터가 비어있는지 확인 (v.size() == 0)보다 많이 사용
-        cout << "PREDAJA";
+        cout << "PREDAJA\n";
     } else {
-        for(char c : v) cout << c;
+        cout << ret << "\n";
     }
 
     return 0;
