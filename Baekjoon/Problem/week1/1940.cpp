469c0e7d99e535c0f34e3afc9915eb54d48e59a1
yeye921 <yewon921@ajou.ac.kr>
2022-11-24 23:49:47 +0900

    주몽

--- Baekjoon/Problem/week1/1940.cpp
+++ Baekjoon/Problem/week1/1940.cpp
@@ -1,36 +1,30 @@
 // 목표: 두개의 수를 더했을 때 m이 나오는 경우의 수는?
 // n개중 2개를 뽑는 경우의 수에 대해서 조사해야함
-// 구하는 법: 1.combination 이용, 2. ..?
+// 구하는 법: 조합 이용
 
-// 조합을 구현하는 방법
-// 1. 이중for문 2. 재귀 3. Permutation 재귀로 해서 앞에 2개 자르기
-// 일단 1번으로 해결해보기
+// 고유한 번호 범위: 1 ~ 10만
+// 두 재료를 더한(=m) 범위: 1 ~ 1000만
+// 고유번호 범위가 10만까지임으로 두개의 합의 범위는 20만까지임
+// 따라서 m값이 20만 ~ 1000만이라면 계산할 필요가 없는 범위이다! => 이게 이 문제의 포인트!
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, num, ret;
-pair<int, int> pi;
-vector<int> v;
+int n, m;
+int a[15004];
 int main(){
-    /*
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    */
     cin >> n;
     cin >> m;
-    for(int i = 0; i < n; i++){
-        cin >> num;
-        v.push_back(num);
-    }
 
-    // n개중에 2개를 뽑는 경우의 수
     for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-            // 뽑은 2개를 더한 값이 m이 되는지 확인
-            if(v[i] + v[j] == m){ // i,j가 인덱스로 이용되는 점 주의하기!
-                ret += 1;
+        cin >> a[i];
+    }
+    if(m > 200000) cout << 0 << "\n"; // !! 계산을 하지 않아도 되는 경우를 if문으로 효율화해야함 (이런 문제도 있음)
+    else { // 여기에는 메인 로직이 들어감 (조합)
+        // n개중에 2개를 뽑는 경우의 수
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(a[i] + a[j] == m) cnt++;
             }
         }
+        cout << cnt << "\n";
     }
-    cout << ret << "\n";
-    return 0;
 }
