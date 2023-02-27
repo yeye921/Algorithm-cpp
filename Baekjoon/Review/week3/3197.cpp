6d4a32df23a79a83d48a080fc34e38c099b27ad8
yeye921 <yewon921@ajou.ac.kr>
2023-02-27 23:59:07 +0900

    다시풀기 1

--- Baekjoon/Review/week3/3197.cpp
+++ Baekjoon/Review/week3/3197.cpp
@@ -1,91 +1,74 @@
-// 플루드필 알고리즘
-// 매일 물 공간과 접촉한 모든 빙판 공간은 녹는다.
-
-// .:물, X:빙판, L:백조
-// 목표: 며칠이 지나야 백조들이 만날 수 있는지 계산
-
-// queue 초기화 하는 법 (빈 큐와 바꿔치기하기)
-// queue<pair<int, int>> empty;
-// swap(기존 큐, empty);
 #include <bits/stdc++.h>
 using namespace std;
-#define y1 aaa
 const int max_n = 1501;
 const int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
 int r, c, y, x, swanY, swanX, day;
-int visited_swan[max_n][max_n], visited[max_n][max_n];
-bool isSwanMeet;
-char _map[max_n][max_n]; // 2000 * 2000 = 4000000 (4백만)
-queue<pair<int, int>> waterQ, water_tempQ, swanQ, swan_tempQ;
-void Qclear(queue<pair<int, int>> &q){
-    queue<pair<int, int>> empty;
+char _map[max_n][max_n];
+bool visited[max_n][max_n], visited_swan[max_n][max_n], isSwanMeet;
+queue<pair<int, int>> swanQ, swan_tempQ, waterQ, water_tempQ;
+void clearQ(queue<pair<int, int>> &q){ // queue 초기화 하는 방법 (큐는 clear함수 없음)
+    queue<pair<int, int>> empty; 
     swap(q, empty);
 }
-void water_melting (){
-    while(waterQ.size()){ // move_swan과 water_melting 모두 맨 앞에 이게 있어야함 !!
-        tie(y, x) = waterQ.front();
-        waterQ.pop();
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx]) continue;
-            if(_map[ny][nx] == 'X'){
-                visited[ny][nx] = 1;
-                water_tempQ.push({ny, nx});
-                _map[ny][nx] = '.';
-            }
-        }
-    }
-}
-// 백조 이동 방식도 물이 녹는 방식과 동일함 (.이면 계속이동하다가 X이면 멈춤 > 다음 턴에 X먼저 방문)
-// move_swan도 tempQ로 처리해야되는구나 !! (주의)
+// 백조도 .을 만나면 계속 탐색하고 X를 만나면 멈춤 > 다음 턴에 X부터 탐색함
 bool move_swan(){
-    while(swanQ.size()){ // 맨 처음에는 하나만 들어있음
-        tie(y, x) = swanQ.front(); 
+    while(swanQ.size()){
+        tie(y, x) = swanQ.front();
         swanQ.pop();
         for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
             if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited_swan[ny][nx]) continue;
-            visited_swan[ny][nx] = 1;
-            if(_map[ny][nx] == '.') swanQ.push({ny, nx}); // 물은 건널 수 있음으로 q에 넣음
-            else if(_map[ny][nx] == 'X') swan_tempQ.push({ny, nx}); // X는 다음 턴에 방문할 수 있으므로 tempQ에 넣음
+            visited_swan[ny][nx] = 1; // 왜 visited 처리를 여기서 해야되지?
+            if(_map[ny][nx] == '.') swanQ.push({ny, nx});
+            else if(_map[ny][nx] == 'X') swan_tempQ.push({ny, nx});
             else if(_map[ny][nx] == 'L') return true;
         }
     }
     return false;
 }
+void water_melting(){
+    while(waterQ.size()){
+        tie(y, x) = waterQ.front();
+        waterQ.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx]) continue; 
+            // 여기다가는 visited[ny][nx] = 1 안넣음 > 왜? 이미 waterQ에 넣으면서 방문처리 했음
+            if(_map[ny][nx] == 'X'){
+                visited[ny][nx] = 1;
+                water_tempQ.push({ny, nx});
+                _map[ny][nx] = '.'; // 다음 턴에 백조가 건너려면 X를 .로 변환 필수 !! (얼음이 물로 녹는다)
+            }
+        }
+    }
+}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     cin >> r >> c;
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
             cin >> _map[i][j];
-            if(_map[i][j] == 'L') {swanY = i, swanX = j;}
-            if(_map[i][j] == '.' || _map[i][j] == 'L') visited[i][j] = 1, waterQ.push({i, j}); // 백조는 왜 waterQ에 넣어야 됨?
+            if(_map[i][j] == 'L'){swanY = i, swanX = j;}
+            if(_map[i][j] == '.' || _map[i][j] == 'L'){ // Q. L이어도 이동할 수 있으니까 넣는건가...?
+                waterQ.push({i, j});
+                visited[i][j] = 1;
+            } 
         }
     }
-    // 백조 이동하기 위한 초기처리
-    swanQ.push({swanY, swanX});
     visited_swan[swanY][swanX] = 1;
-
-    // 로직
-    // 1. 백조가 만날 수 있는지 확인
-    // 2. 물과 인접한 빙판이 녹는다
-
-    // (이전에 탐색한 것을 패스하고 녹은 것 부터 탐색해야함 !!)
-    // .이면 계속 탐색 x를 만나면 tempq에 넣고 탐색 종료
-    // 이렇게 하려면 .인 것들을 이미 q에다 넣어놓고 .을 기반으로 탐색해야함
-    while(true){ // 이 구조 기억하기
+    swanQ.push({swanY, swanX});
+    
+    while(1){
         if(move_swan()) break;
-        water_melting();
+        water_melting();    
         waterQ = water_tempQ;
         swanQ = swan_tempQ;
-
-        Qclear(water_tempQ);
-        Qclear(swan_tempQ);
+    
+        clearQ(water_tempQ);
+        clearQ(swan_tempQ);
         day++;
     }
     cout << day << "\n";
---
+++ Exam/SkPlanet/ex.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s = "abbc";
+int main(){
+    auto it = s.find("b");
+    cout << it << "\n";
+}
