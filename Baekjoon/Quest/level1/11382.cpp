41dcbcd25215245586c2ef30c26da4b6d57c2855
yeye921 <yewon921@ajou.ac.kr>
2023-03-27 22:03:07 +0900

    꼬마 정민

---
+++ Baekjoon/Quest/level1/11382.cpp
@@ -0,0 +1,9 @@
+// int형 정수는 +- 20억까지 저장가능. 20억 = 10^9 (10자리 숫자)
+// long long형 정수는 1e18=10^18까지 저장가능. 
+#include <bits/stdc++.h>
+using namespace std;
+long long a, b, c;
+int main(){
+    cin >> a >> b >> c;
+    cout << a + b + c << "\n"; 
+}
