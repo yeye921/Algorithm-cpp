9ba8d9b9d5725c4be470d201575c450489b39496
yeye921 <yewon921@ajou.ac.kr>
2023-03-28 12:21:48 +0900

    강아지

---
+++ Baekjoon/Quest/level1/10172.cpp
@@ -0,0 +1,11 @@
+// ""(큰따옴표)를 출력하기 위해서는 \를 붙여줘야 한다
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "|\\_/|" << "\n";
+    cout << "|q p|   /}" << "\n";
+    cout << "( 0 )\"\"\"\\" << "\n";
+    cout << "|\"^\"`    |" << "\n";
+    cout << "||_/=\\\\__|" << "\n";
+    return 0;
+}
