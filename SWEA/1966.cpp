af9f3fa51b5996ee279c7c323394421a4b64a6ad
yeye921 <yewon921@ajou.ac.kr>
2023-04-30 00:01:16 +0900

    숫자를 정렬하자

--- SWEA/1966.cpp
+++ SWEA/1966.cpp
@@ -1,5 +1,6 @@
 // n 길이의 숫자열을 오름차순으로 정렬하여 출력하는 문제
 // m1) 벡터에 숫자들을 넣고 정렬 함수 이용함
+// 주의) 벡터는 memset으로 초기화 못하고 fill로만 할 수 있다!
 #include <iostream>
 #include<vector>
 #include<algorithm>
@@ -8,8 +9,8 @@ int T, test_case;
 int n, num;
 int main(){
     cin >> T;
-    for(int test_case = 1; test_case <= T; test_case++){
-        vector<int> v;
+    for(int test_case = 1; test_case <= T; test_case++){ // 테스트케이스는 초기화가 중요!
+        vector<int> v; // 여기서 선언하는게 중요!!
         cin >> n;
         for(int i = 0; i < n; i++){
             cin >> num;
