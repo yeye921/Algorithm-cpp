66bea37944a948a6caa946327ec918ac9d1ebb0f
yeye921 <yewon921@ajou.ac.kr>
2023-04-30 20:26:17 +0900

    중간 평균값 구하기

--- SWEA/1984.cpp
+++ SWEA/1984.cpp
@@ -1,6 +1,5 @@
 // 10개의 수를 입력 받아, 최대 수와 최소 수를 제외한 나머지의 평균값을 출력하는 문제
-// m1) 벡터안에 든 값중 최대, 최소값을 구하는 함수를 통해 구한뒤 총합에서 빼주고 처리함
-// m2) 벡터에 값을 다 담고 정렬한 뒤, 맨 앞값과 맨 뒤값을 제외한 합의 평균을 출력
+// : 최대, 최소값을 구한 뒤 빼주고 평균 계산하기 (수의 개수는 10개로 고정됨)
 
 // 평균값을 반올림하기위해 round함수 사용
 #include <iostream>
@@ -13,17 +12,18 @@ int n;
 int main(){
     cin >> T;
     for(int test_case = 1; test_case <= T; test_case++){
-        vector<int> v;
         double ret = 0;
+        // 최대값은 최소값부터, 최소값은 최대값부터
+        int mx = 0;
+        int mn = 10000;
         for(int i = 0; i < 10; i++){
             cin >> n;
-            ret += n; // 나중에 여기서 최대, 최소값 빼기
-            v.push_back(n);
+            ret += n;
+            if(n > mx) mx = n;
+            if(n < mn) mn = n;
         }
-        int mx = *max_element(v.begin(), v.end());
-        int mn = *min_element(v.begin(), v.end());
         ret -= mx + mn;
-        ret /= (v.size() - 2);
+        ret /= 8;
         cout << "#" << test_case << " " << round(ret) << "\n";
     }
 }
