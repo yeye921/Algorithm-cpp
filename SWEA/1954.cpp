039d4e74ae1e961e3aec45e59ca5d6f70b7a615f
yeye921 <yewon921@ajou.ac.kr>
2023-05-06 22:12:35 +0900

    달팽이 숫자

--- SWEA/1954.cpp
+++ SWEA/1954.cpp
@@ -1,62 +1,41 @@
 // 정수 n을 입력 받아, N크기의 달팽이 출력하기
-// 달팽이 풀이 1) 재귀 함수 이용 
-// ex. 4*4 -> 2*2 -> 1*1로 쪼개서 겉 테두리만 채우면 됨
+// 힌트: dfs와 방향벡터 이용
 
+// 규칙1) 범위 이내일때만 작업함 (범위를 벗어나면 방향이 바뀜)
+// 규칙2) 배열 값이 0인 경우(값이 비어 있을때)만 작업함, 값이 이미 들어있는 경우에는 방향을 전환함
+// 규칙3) 적을 값(cnt)가 n^2 이하일 때까지만 이 작업을 수행한다. 
+// 방향의 종류는 4가지이고 반복/순환함 (오른쪽, 아래, 왼쪽, 위)
 #include <iostream>
 using namespace std;
-const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
 int T, test_case;
 int n;
 int _map[13][13];
-// 재귀함수
-void recursion(){
-    // 여기서 쪼개고 어쩌고 ...
-}
+const int dy[4] = {0, 1, 0, -1}, dx[4] = {1, 0, -1, 0};
 int main(){
     cin >> T;
     for(int test_case = 1; test_case <= T; test_case++){
-        fill(&_map[0][0], &_map[13][13], 0); // 2차원 배열 초기화
+        memset(_map, 0, sizeof(_map));
+        int y = 0, x = 0;
+        int cnt = 1;
+        int dir = 0;
         cin >> n;
-        int size = n;
-        int num = 1;
-        int start = 0;
-        // 일단 겉 테두리만 돌기 (0행, n-1열, n-1행, 0열)
-        while(size != 1){
-            // 0행 (x+1)
-            for(int i = start; i < n; i++){
-                if(!_map[start][i]){ // 값이 비어있을 때만 실행함
-                    _map[start][i] = num;
-                    num++;
-                }
-            }
-            // n-1열 (y+1)
-            for(int i = start; i < n; i++){
-                if(!_map[i][n-1]){ // 값이 비어있을 때만 실행함
-                    _map[i][n-1] = num;
-                    num++;
-                }
-            }
-            // n-1행 (x-1)
-            for(int i = n-1; i >= start; i--){
-                if(!_map[n-1][i]){
-                    _map[n-1][i] = num;
-                    num++;
-                }
-            }
-            // // 0열 (y-1)
-            for(int i = n-1; i >= start; i--){
-                if(!_map[i][start]){
-                    _map[i][start] = num;
-                    num++;
-                }
-            }
-            size /= 2;
-            start++;
-        }
+        while(cnt != n*n+1){ // 카운트가 n제곱+1이 되면 멈춤
+            _map[y][x] = cnt;
 
+            int ny = y + dy[dir % 4];
+            int nx = x + dx[dir % 4];
+
+            // 다음 지점이 범위를 벗어나거나 이미 값이 들어있으면 방향을 전환함
+            if((nx < 0 || ny < 0 || nx >= n || ny >= n) || _map[ny][nx]){
+                dir++;
+                ny = y + dy[dir % 4];
+                nx = x + dx[dir % 4];
+            }
+            y = ny;
+            x = nx;
+            cnt++;
+        }
         cout << "#" << test_case << "\n";
-        // 달팽이 출력
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 cout << _map[i][j] << " ";
