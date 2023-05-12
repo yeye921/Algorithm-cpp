fafa164542bc00f68ad484795a6b90daaa469275
yeye921 <yewon921@ajou.ac.kr>
2023-05-12 23:49:47 +0900

    [S/W 문제해결 응용] 2일차 - 최대 상금

---
+++ SWEA/D3/1244.cpp
@@ -0,0 +1,14 @@
+// 주어진 숫자판들 중에 두개를 선택해서 정해진 횟수만큼 서로의 자리 위치를 교환할 수 있다
+// 반드시 주어진 횟수만큼 교환이 이루어져야 하고 동일한 위치의 교환이 중복되어도 된다
+// => 즉, 이리저리 정해진 횟수만큼 수를 교환해서 받을 수 있는 가장 큰 금액 계산
+#include <iostream>
+using namespace std;
+int T;
+int num, cnt, ret;
+int main(){
+    cin >> T;
+    for(int test_case = 1; test_case <= T; test_case++){
+        c
+        cout << "#" << test_case << " ";
+    }
+}
