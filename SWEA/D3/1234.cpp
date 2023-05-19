225cfd005875acd0e45535675a1a30aca659f839
yeye921 <yewon921@ajou.ac.kr>
2023-05-19 23:50:56 +0900

    [S/W 문제해결 기본] 10일차 - 비밀번호

---
+++ SWEA/D3/1234.cpp
@@ -0,0 +1,28 @@
+// 번호 문자열에서 같은 번호로 붙어있는 쌍들을 소거하고 남은 번호를 비밀번호로 만든다
+// 번호 쌍이 소거되고 소거된 번호 쌍의 좌우 번호가 같은 번호면 또 소거할 수 있다
+// m1) stack 이용
+#include <iostream>
+#include <stack>
+using namespace std;
+int n;
+string s;
+stack<int> st;
+int main(){
+    for(int tc = 1; tc <= 10; tc++){
+        cin >> n >> s;
+        for(int i = 0; i < n; i++){ // 문자열의 문자 탐색
+            int now = s[i] - '0';
+            if(st.size() && st.top() == now){ // 스택의 탑에 동일한 번호가 존재하면 제거
+                st.pop();
+            }
+            else st.push(now);
+        }
+        cout << "#" << tc << " ";
+        string ret;
+        while(st.size()){
+            cout << st.top();
+            st.pop();
+        }
+        cout << "\n";
+    }
+}
