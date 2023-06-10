b9f1ec2c533ceec1bf27efbd6c6364fddadb5917
yeye921 <yewon921@ajou.ac.kr>
2023-06-10 20:57:09 +0900

    A+B - 4

---
+++ Baekjoon/Quest/level3/10951.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    while(cin >> a >> b){
+        cout << a + b << "\n";
+    }
+}
