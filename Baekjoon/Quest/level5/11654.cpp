299d7c6708eec6a8d37fe954125e71257b013209
yeye921 <yewon921@ajou.ac.kr>
2023-06-13 09:50:16 +0900

    아스키 코드

---
+++ Baekjoon/Quest/level5/11654.cpp
@@ -0,0 +1,8 @@
+// 주어진 글자의 아스키 코드 값을 출력하는 문제
+#include <bits/stdc++.h>
+using namespace std;
+char ch;
+int main(){
+    cin >> ch;
+    cout << (int)ch << "\n"; // 문자char타입을 정수int형에 넣으면 아스키코드로 저장된다
+}
